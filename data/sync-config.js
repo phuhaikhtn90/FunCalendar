e7de9fb4e01b0a167961682e937c399ad5f5a497
Hai Le <phuhaikhtn90@gmail.com>
2026-05-20 23:00:45 +0700

    - update sync-config

--- data/sync-config.js
+++ data/sync-config.js
@@ -1,6 +1,6 @@
 window.FUN_CALENDAR_SYNC_CONFIG = {
-  enabled: false,
+  enabled: true,
   provider: "google-apps-script",
-  endpointUrl: "",
-  accessKey: ""
+  endpointUrl: "https://script.google.com/macros/s/AKfycbz5it27F1AWQ1tRGCdiaTAKO9SCEhfjuA21z2G-n5l9PqUet6CRQuyOMqukTEMXGbEWtg/exec",
+  accessKey: "my-fun-calendar-2026"
 };
